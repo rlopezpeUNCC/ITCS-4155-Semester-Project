73bdb74979c9a87a1b94a8ebf921b3cb3481bf7f
rlopezpeUNCC <71346643+rlopezpeUNCC@users.noreply.github.com>
2021-09-02 17:01:13 -0400

    Added test object

--- PC Component App/Assets/Scripts/Test.cs
+++ PC Component App/Assets/Scripts/Test.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class Test : MonoBehaviour {
+
     void Start()
     {
         
---
+++ PC Component App/Assets/Scripts/newScrip.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class newScrip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+
+}
