3e9927c675a20ed982ee59041259277013e4bc36
rlopezpeUNCC <71346643+rlopezpeUNCC@users.noreply.github.com>
2021-09-21 16:03:17 -0400

    9/21/21 meeting tutorial

---
+++ PC Component app (1)/Assets/bringUpMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class bringUpMenu : MonoBehaviour {
+    [SerializeField]
+    GameObject menu;
+    [SerializeField]
+    TextMeshProUGUI text;
+
+    public void ShowMenu() { 
+        if (menu.active) {
+            menu.SetActive(false);
+        } else {
+            menu.SetActive(true);
+        }        
+    }
+
+    public void HideMenu() {
+        text.enabled = false;
+    }
+
+}
