a94d8b8d76cf0b0a165372526f99441314c81437
rlopezpeUNCC <71346643+rlopezpeUNCC@users.noreply.github.com>
2021-10-02 13:02:13 -0400

    Fixed a bug in camera zooming caused by last commit

--- PC Component App/Assets/Scripts/wheelZoom.cs
+++ PC Component App/Assets/Scripts/wheelZoom.cs
@@ -4,23 +4,25 @@ public class wheelZoom : MonoBehaviour{
 
 	float max = 10.0F;
 	float min = 1.0F;
-	float speed = Time.deltaTime * 10.0F;
-	Camera camera;
+	float speed;
+	Camera mainCamera;
 	
 	private void Start(){
-		camera = Camera.main;
+		speed = Time.deltaTime * 10.0F;
+		mainCamera = Camera.main;
 	}	
 
 	private void Update(){	
-		if (camera.orthographic){
-			if ((Input.GetAxis("Mouse ScrollWheel") > 0) & (camera.orthographicSize > min)){
-				camera.orthographicSize -= speed;
+		if (mainCamera.orthographic){
+			if ((Input.GetAxis("Mouse ScrollWheel") > 0) & (mainCamera.orthographicSize > min)){
+				Debug.Log("zooming");
+				mainCamera.orthographicSize -= speed;
 			}
-			else if ((Input.GetAxis("Mouse ScrollWheel") < 0) & (camera.orthographicSize < max)){
-				camera.orthographicSize += speed;
+			else if ((Input.GetAxis("Mouse ScrollWheel") < 0) & (mainCamera.orthographicSize < max)){
+				mainCamera.orthographicSize += speed;
 			}
 			else{
-				camera.orthographicSize += 0.0F;
+				mainCamera.orthographicSize += 0.0F;
 			}
 		}
 	}
