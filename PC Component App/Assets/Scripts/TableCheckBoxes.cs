e5a5b8aa4f7e6e3006c954b9a0c0d2a9a3b0ae8e
aoitori-asd <59700405+aoitori-asd@users.noreply.github.com>
2021-10-06 16:28:42 -0400

    Added listener code to table of contents
    - Each ToC toggle receives a listener to detect when un/checked

---
+++ PC Component App/Assets/Scripts/TableCheckBoxes.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+// Currently attached to ToCPanel
+// Uses foreach code from https://forum.unity.com/threads/need-help-with-toggle.450210/#post-2913916
+
+public class TableCheckBoxes : MonoBehaviour
+{
+    private List<Toggle> tocToggles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Make list of each Toggle in ToCPanel
+        tocToggles = GetComponentsInChildren<Toggle>().ToList();
+
+        // Add listener to each Toggle to know when un/checked
+        foreach(Toggle thisToggle in tocToggles) {
+            thisToggle.onValueChanged.AddListener(delegate {
+                ToggleValueChanged(thisToggle);
+            });
+            print("Listener added for checkbox " + thisToggle.name);
+        }
+    }
+
+    // TODO: Show/hide computer part when its respective toggle is clicked
+    void ToggleValueChanged(Toggle thisToggle)
+    {
+        print("Checkbox " + thisToggle.name + " state = " + thisToggle.isOn);
+    }
+}
