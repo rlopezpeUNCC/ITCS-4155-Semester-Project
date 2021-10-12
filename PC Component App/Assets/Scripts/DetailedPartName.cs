eb47785cf238ad16a52cd837a11695bc80a3de4a
aoitori-asd <59700405+aoitori-asd@users.noreply.github.com>
2021-10-12 16:57:35 -0400

    Added show/hide functionality and detailed part names
    - Un/checking a ToC entry now shows/hides the respective part
    - Updated part names in Computer model to match ToC names, simplifying ToC functionality
    - Added DetailedPartName script to preserve detailed part names for future versions
    - Added TODO to TableCheckBoxes related to above

---
+++ PC Component App/Assets/Scripts/DetailedPartName.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+/* 
+    Currently attached to all immediate children of Computer GameObject.
+    
+    Adds text field in Inspector to give each computer part a detailed name.
+    This allows us to keep the model names generic in the hierarchy but give a more specific name for the user.
+    This simplifies the part-tracking process for script TableCheckBoxes while allowing us to keep the "true" part name.
+
+    Uses code from Unity3D documentation: https://docs.unity3d.com/Manual/VariablesAndTheInspector.html
+*/
+public class DetailedPartName : MonoBehaviour 
+{
+    public string partName;
+    
+    // Use this for initialization
+    void Start () 
+    {
+        Debug.Log("Part name is " + partName);
+    }
+}
--- PC Component App/Assets/Scripts/TableCheckBoxes.cs
+++ PC Component App/Assets/Scripts/TableCheckBoxes.cs
@@ -4,14 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 // Currently attached to ToCPanel
 // Uses foreach code from https://forum.unity.com/threads/need-help-with-toggle.450210/#post-2913916
+// TODO: Add code to update text for individual Toggles to show the partName of their respective Transform in tocParts.
 
 public class TableCheckBoxes : MonoBehaviour
 {
-    private List<Toggle> tocToggles;
+    private List<Toggle> tocToggles; // list of checkboxes in ToC
+    Dictionary<string, Transform> tocParts; // dictionary of parts named by each toggle
 
     // Start is called before the first frame update
     void Start()
     {
+        // Make dictionary of parts in current computer
+        GameObject parentComputer = GameObject.Find("Computer");
+        tocParts = new Dictionary<string, Transform>();
+        foreach (Transform child in parentComputer.transform)
+        {
+            tocParts.Add(child.name, child);
+            print("Added to tocParts: " + child.name);
+        }
+
         // Make list of each Toggle in ToCPanel
         tocToggles = GetComponentsInChildren<Toggle>().ToList();
 
@@ -24,10 +35,25 @@ public class TableCheckBoxes : MonoBehaviour
         }
     }
 
-    // TODO: Show/hide computer part when its respective toggle is clicked
+    // Show/hide computer part when its respective toggle is clicked
     void ToggleValueChanged(Toggle thisToggle)
     {
+        // Find the tocParts member with the same name as the clicked toggle, then gather its children
+        Renderer[] lChildRenderers=tocParts[thisToggle.name].GetComponentsInChildren<Renderer>();
+
+        // Show (or hide) each child until the part is fully shown/hidden
+        if (thisToggle.isOn) { // Checked (Show)
+            foreach ( Renderer lRenderer in lChildRenderers)
+            {
+                lRenderer.enabled=true;
+            }
+        } else { // Unchecked (Hide)
+            foreach ( Renderer lRenderer in lChildRenderers)
+            {
+                lRenderer.enabled=false;
+            }
+        }
         FindObjectOfType<AudioManager>().Play("ButtonClicked2");
-        print("Checkbox " + thisToggle.name + " state = " + thisToggle.isOn);
+        //print("Checkbox " + thisToggle.name + " state = " + thisToggle.isOn);
     }
 }
