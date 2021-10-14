7cdb2634a2f07d21e91b2d454386e3dbe4e13028
harsh002 <harshthakor02@gmail.com>
2021-10-14 15:30:20 -0400

    Started working on component menu
    Detail Menu complete

--- PC Component App/Assets/Scripts/ComponentMenu.cs
+++ PC Component App/Assets/Scripts/ComponentMenu.cs
@@ -9,8 +9,16 @@ public class ComponentMenu : MonoBehaviour
     [SerializeField]
     GameObject menu;
 
+    //one for each component
    componentDetail CPU = new componentDetail();
-   //moyher
+   componentDetail GraphicsCard = new componentDetail();
+   componentDetail CPUCooler = new componentDetail();
+   componentDetail RAM = new componentDetail();
+   componentDetail Case = new componentDetail();
+   componentDetail CaseCooling = new componentDetail();
+   componentDetail Motherboard = new componentDetail();
+   componentDetail PowerSupply = new componentDetail();
+   
 
 
    public struct componentDetail
@@ -24,16 +32,48 @@ public class ComponentMenu : MonoBehaviour
     void Start()
     {
         
-
+// all 3 one for each
         CPU.name = "name";
         CPU.description = "description";
         CPU.price = "price";
 
-//mother
+        GraphicsCard.name = "name";
+        GraphicsCard.description = "description";
+        GraphicsCard.price = "price";
+
+        CPUCooler.name = "name";
+        CPUCooler.description = "description";
+        CPUCooler.price = "price";
+
+        RAM.name = "name";
+        RAM.description = "description";
+        RAM.price = "price";
+
+        Case.name = "name";
+        Case.description = "description";
+        Case.price = "price";
+
+        CaseCooling.name = "name";
+        CaseCooling.description = "description";
+        CaseCooling.price = "price";
+
+        Motherboard.name = "name";
+        Motherboard.description = "description";
+        Motherboard.price = "price";
+
+        PowerSupply.name = "name";
+        PowerSupply.description = "description";
+        PowerSupply.price = "price";
+
 
         DetailSetup("CPU");
-
-
+        DetailSetup("GraphicsCard");
+        DetailSetup("CPUCooler");
+        DetailSetup("RAM");
+        DetailSetup("Case");
+        DetailSetup("CaseCooling");
+        DetailSetup("Motherboard");
+        DetailSetup("PowerSupply");
 
     }
 
@@ -44,25 +84,54 @@ public class ComponentMenu : MonoBehaviour
 
             case ("CPU"):
                 componentName.text = CPU.name;
-                //des
-                //price
+                componentName.text = CPU.description;
+                componentName.text = CPU.price;
 
                 break;
 
-            case ("motherboard"):
-                //componentName.text = motherboard.name;
+            case ("Motherboard"):
+                componentName.text = Motherboard.name;
+                componentName.text = Motherboard.description;
+                componentName.text = Motherboard.price;
                 break;
 
+            case ("GraphicsCard"):
+                componentName.text = GraphicsCard.name;
+                componentName.text = GraphicsCard.description;
+                componentName.text = GraphicsCard.price;
+                break;
 
+            case ("CPUCooler"):
+                componentName.text = CPUCooler.name;
+                componentName.text = CPUCooler.description;
+                componentName.text = CPUCooler.price;
+                break;
 
+            case ("RAM"):
+                componentName.text = RAM.name;
+                componentName.text = RAM.description;
+                componentName.text = RAM.price;
+                break;
 
+            case ("Case"):
+                componentName.text = Case.name;
+                componentName.text = Case.description;
+                componentName.text = Case.price;
+                break;
 
+            case ("CaseCooling"):
+                componentName.text = CaseCooling.name;
+                componentName.text = CaseCooling.description;
+                componentName.text = CaseCooling.price;
+                break;
 
+             case ("PowerSupply"):
+                componentName.text = PowerSupply.name;
+                componentName.text = PowerSupply.description;
+                componentName.text = PowerSupply.price;
+                break;
 
         }
-
-        
-
 
     }
 
