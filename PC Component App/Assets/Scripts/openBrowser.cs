3f44217798695872fb6adfd234a8a3f5077ec37e
mgibbon7 <77972351+mgibbon7@users.noreply.github.com>
2021-10-21 14:44:09 -0400

    Webscraper and open web page

---
+++ PC Component App/Assets/Scripts/openBrowser.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openBrowser : MonoBehaviour{
+
+    void Start(){
+
+
+    }
+    public void openGoogleButton(){
+
+        Application.OpenURL("www.google.com");
+    }
+
+    void Update(){
+
+
+    }
+}
