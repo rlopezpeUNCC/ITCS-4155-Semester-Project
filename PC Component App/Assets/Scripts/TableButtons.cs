65fbbe910396241b4585a90af24ac50005076762
rlopezpeUNCC <71346643+rlopezpeUNCC@users.noreply.github.com>
2021-10-25 16:33:39 -0400

    Added animation for TOC menu

--- PC Component App/Assets/Scripts/TableButtons.cs
+++ PC Component App/Assets/Scripts/TableButtons.cs
@@ -9,29 +9,39 @@ public class TableButtons : MonoBehaviour
 {
     [SerializeField]
     ComponentMenu menu;     // lets us use ComponentMenu functions
+    [SerializeField]
+    Animator arrowAnimator;
 
-    private List<Button> tocButtons;    // list of buttons in ToC
+    List<Button> tocButtons;    // list of buttons in ToC
     // from TableCheckBoxes: public Dictionary<string, Transform> tocParts;
 
     // Start is called before the first frame update
     void Start()
     {
         // Make list of each Button in ToCPanel
-        tocButtons = GetComponentsInChildren<Button>().ToList();
-
+        tocButtons = GetComponentsInChildren<Button>().ToList();        
         // Add listener to each Button to know when clicked
         foreach(Button thisButton in tocButtons) {
-            thisButton.onClick.AddListener(delegate {
-                ButtonClicked(thisButton);
-            });
+            if (thisButton.name != "ToCShowHide") {
+                thisButton.onClick.AddListener(delegate {
+                    ButtonClicked(thisButton);
+                });
+            }
            // print("Listener added for button " + thisButton.name);
         }
     }
 
     // TODO: Highlight computer part when its respective ToC Button is clicked and pull up context menu
-    void ButtonClicked(Button thisButton)
-    {
-        //print("Button clicked for " + thisButton.GetComponentInParent<Toggle>().name);
+    void ButtonClicked(Button thisButton) {
+        //print("Button clicked for " + thisButton.GetComponentInParent<Toggle>().name);        
         menu.DetailSetup(thisButton.GetComponentInParent<Toggle>().name);
+    }
+
+    public void OpenCloseMenu() {
+        Animator animator = gameObject.GetComponent<Animator>();        
+        bool isOpen = animator.GetBool("open");
+        animator.SetBool("open", !isOpen);
+        arrowAnimator.SetBool("open", isOpen);
+
     }
 }
