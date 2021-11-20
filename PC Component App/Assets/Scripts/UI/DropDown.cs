0ed5dc8e802f85a984f24794bb229e011c895270
rlopezpeUNCC <rlopezpe@uncc.edu>
2021-11-19 22:37:46 -0500

    Started played around with reading from database (unsuccessfully) :(

--- PC Component App/Assets/Scripts/UI/DropDown.cs
+++ PC Component App/Assets/Scripts/UI/DropDown.cs
@@ -1,11 +1,34 @@
-using UnityEngine;
+using UnityEngine; 
 using System.Collections.Generic;
+using System.Data;
+//using Mono.Data.Sqlite;
+using System.IO;
+
 public class DropDown : MonoBehaviour {
     [SerializeField]
     TMPro.TMP_Dropdown dropdown;
     [SerializeField]
     ComponentMenu componentMenu;
     string selectedComponent = "Default";
+
+    void Start() {/*
+        string connection = "URI=file:" + Application.dataPath + "/Scraper_Files/Databases/Cases.db";
+        IDbConnection dbcon = new SqliteConnection(connection);
+        dbcon.Open();
+
+        IDbCommand cmnd_read = dbcon.CreateCommand();
+        IDataReader reader;
+        string query ="SELECT * FROM Cases";
+        cmnd_read.CommandText = query;
+        reader = cmnd_read.ExecuteReader();
+        while (reader.Read()){
+            Debug.Log("Name: " + reader[0].ToString());
+            Debug.Log("Cost: " + reader[1].ToString());
+        }
+        dbcon.Close();
+        */
+    }
+
     void Update() {
         if (componentMenu.GetName() != selectedComponent) {
             selectedComponent = componentMenu.GetName();
