a26f1570d7ee73fd86d74472a1427ce6eebb6ad9
aoitori-asd <59700405+aoitori-asd@users.noreply.github.com>
2021-11-29 12:42:49 -0500

    Added music toggle button and functionality
    - It's mildly janky
    - Will need adjustment to match UI
    - Placeholder are in there for future persistent settings, if we add that later on

--- PC Component App/Assets/Scripts/AppManager/AudioManager.cs
+++ PC Component App/Assets/Scripts/AppManager/AudioManager.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System;
+using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour {
-
     public Sound[] sounds;
     public static AudioManager instance;
+    // music-specific vars
+    public bool musicOn; // redundant, but keeping in case we implement long-term saving of muting decisions thru player data
+    private Sound m;
+    private string songName;
+    [SerializeField]
+    Image musicToggleImg;
+    [SerializeField]
+    Sprite onTexture, offTexture;
+
     void Awake() {
         if (instance == null) 
             instance = this;
@@ -14,6 +25,7 @@ public class AudioManager : MonoBehaviour {
             return;
         }
         //DontDestroyOnLoad(gameObject);
+        musicOn = true;
         
         foreach (Sound s in sounds) {
             s.source = gameObject.AddComponent<AudioSource>();
@@ -25,14 +37,40 @@ public class AudioManager : MonoBehaviour {
             }            
             s.source.loop = s.loop;
         }
+
+        // Prepare/play appropriate music for scene
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "Main Menu") {
+            songName = "MainMenuMusic";
+            Play(songName);
+        } else if (scene.name == "Tutorial" || scene.name == "Main Scene") {
+            songName = "BuilderMusic";
+            Play(songName);
+        }
     }
 
     public void Play(string name) {
         Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s == null)  {
+        if (s == null)  { // if sound doesn't exist
             Debug.LogWarning("Sound: " + name + " not found!");
             return;
+        } else if (name == songName) { // if sound is music
+            m = s;
+            m.source.Play();
+        } else { // if sound is sfx
+            s.source.Play();
         }
-        s.source.Play();
+    }
+
+    public void ToggleMusic() {
+        if (musicOn) {
+            m.source.Stop();
+            musicOn = false;
+            musicToggleImg.sprite = offTexture;
+        } else {
+            m.source.Play();
+            musicOn = true;
+            musicToggleImg.sprite = onTexture;
+        }
     }
 }
