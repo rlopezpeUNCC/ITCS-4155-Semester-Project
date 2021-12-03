e28e5dd43bfe956ebefa0d873633990fd067587f
josephc2195 <chicaj1221@gmail.com>
2021-12-03 00:58:36 -0500

    update to mini game
    Added colors for buttons, computer part moves to motherboard when correct button is selected

--- PC Component App/Assets/Scripts/Mini Game/FirstLevel.cs
+++ PC Component App/Assets/Scripts/Mini Game/FirstLevel.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FirstLevel : MonoBehaviour
 {
-    public GameObject ramHandler, cpuHandler, cpuFanHandler, gpuHandler;
-    public Color wrongColor, rightColor;
+    public GameObject cpu, cpuFan, ram, gpu, ramHandler, cpuHandler, cpuFanHandler, gpuHandler;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +19,11 @@ public class FirstLevel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray, out hit, 100f))
+            if (Physics.Raycast(ray, out hit, 100f))
             {
                 if (hit.transform)
                 {
@@ -34,7 +35,7 @@ public class FirstLevel : MonoBehaviour
 
     public void ChangeButtons(string component)
     {
-        if (component == "CPU") 
+        if (component == "CPU")
         {
             ramHandler.SetActive(false);
             cpuFanHandler.SetActive(false);
@@ -42,21 +43,21 @@ public class FirstLevel : MonoBehaviour
             cpuHandler.SetActive(true);
 
         }
-        else if(component == "CPU Fan")
+        else if (component == "CPU Fan")
         {
             cpuHandler.SetActive(false);
             gpuHandler.SetActive(false);
             ramHandler.SetActive(false);
             cpuFanHandler.SetActive(true);
         }
-        else if(component == "RAM")
+        else if (component == "RAM")
         {
             cpuFanHandler.SetActive(false);
             cpuHandler.SetActive(false);
             gpuHandler.SetActive(false);
             ramHandler.SetActive(true);
         }
-        else if(component == "GPU")
+        else if (component == "GPU")
         {
             ramHandler.SetActive(false);
             cpuFanHandler.SetActive(false);
@@ -66,6 +67,37 @@ public class FirstLevel : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        
+
+    }
+
+    public void ButtonClicked(Button thisButton)
+    {
+        if (thisButton.name == "Correct")
+        {
+            print("Correct");
+            ramHandler.SetActive(false);
+            cpuFanHandler.SetActive(false);
+            cpuHandler.SetActive(false);
+            gpuHandler.SetActive(false);
+            print("Parent name: " + thisButton.transform.parent.name);
+            string parentName = thisButton.transform.parent.name;
+            if (parentName == "CPU Button Canvas")
+            {
+                cpu.transform.position = new Vector3(-1.21f, .07f, 6.275f);
+            }
+            else if(parentName == "RAM Button Canvas")
+            {
+                ram.transform.position = new Vector3(.82f, .08f, 7.37f);
+            }
+            else if(parentName == "CPU Fan Button Canvas")
+            {
+                cpuFan.transform.position = new Vector3(-1.21f, .07f, 6.5f);
+            }
+            else if(parentName == "GPU Button Canvas")
+            {
+                gpu.transform.position = new Vector3(-0.3f, -.04f, 5.2f);
+            }
+        }
     }
 }
+
