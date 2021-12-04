8ef91994e35e5279b17886195a52779891994ddf
aoitori-asd <59700405+aoitori-asd@users.noreply.github.com>
2021-12-04 13:01:08 -0500

    Added GUI and points system to minigame

--- PC Component App/Assets/Scripts/Mini Game/FirstLevel.cs
+++ PC Component App/Assets/Scripts/Mini Game/FirstLevel.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class FirstLevel : MonoBehaviour
 {
-    public GameObject cpu, cpuFan, ram, gpu, ramHandler, cpuHandler, cpuFanHandler, gpuHandler;
+    public GameObject cpu, cpuFan, ram, gpu, ramHandler, cpuHandler, cpuFanHandler, gpuHandler, introPanel, sidePanel;
+    public TextMeshProUGUI scoreField;
+    private int score;
 
     // Start is called before the first frame update
     void Start()
@@ -14,9 +17,12 @@ public class FirstLevel : MonoBehaviour
         cpuFanHandler.SetActive(false);
         cpuHandler.SetActive(false);
         gpuHandler.SetActive(false);
+        introPanel.SetActive(true);
+        sidePanel.SetActive(false);
+        score = 0;
     }
 
-    // Update is called once per frame
+    // Update is called once per frame, detects which part user wants to place
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -33,6 +39,13 @@ public class FirstLevel : MonoBehaviour
         }
     }
 
+    // Hides instructions so user can play game (not to be confused with the Start() method above!!!)
+    public void StartLevel() {
+        introPanel.SetActive(false);
+        sidePanel.SetActive(true);
+    }
+
+    // Displays right/wrong choices for part placement
     public void ChangeButtons(string component)
     {
         if (component == "CPU")
@@ -65,36 +78,34 @@ public class FirstLevel : MonoBehaviour
             gpuHandler.SetActive(true);
         }
     }
-    private void OnMouseDown()
-    {
 
-    }
-
+    // If correct placement is chosen, place the part there and add points; otherwise, they lose points
     public void ButtonClicked(Button thisButton)
     {
-        if (thisButton.name == "Correct")
-        {
-            ramHandler.SetActive(false);
-            cpuFanHandler.SetActive(false);
-            cpuHandler.SetActive(false);
-            gpuHandler.SetActive(false);
+        if (thisButton.name == "Correct") { // Right choice made
+            // Gain points
+            score += 10;
+            scoreField.SetText(score.ToString());
+
+            // Hide choice buttons and place part
             string parentName = thisButton.transform.parent.name;
-            if (parentName == "CPU Button Canvas")
-            {
+            if (parentName == "CPU Button Canvas") {
+                cpuHandler.SetActive(false);
                 cpu.transform.position = new Vector3(-1.21f, .07f, 6.275f);
-            }
-            else if(parentName == "RAM Button Canvas")
-            {
+            } else if (parentName == "RAM Button Canvas") {
+                ramHandler.SetActive(false);
                 ram.transform.position = new Vector3(.82f, .08f, 7.37f);
-            }
-            else if(parentName.Trim() == "Fan Button Canvas")
-            {
+            } else if (parentName.Trim() == "Fan Button Canvas") {
+                cpuFanHandler.SetActive(false);
                 cpuFan.transform.position = new Vector3(-1.12f, 2.13f, 5.4f);
-            }
-            else if(parentName == "GPU Button Canvas")
-            {
+            } else if (parentName == "GPU Button Canvas") {
+                gpuHandler.SetActive(false);
                 gpu.transform.position = new Vector3(-0.3f, -.04f, 5.2f);
             }
+        } else { // wrong choice made
+            score -= 5;
+            if (score < -999) { score = -999 ;} // prevent errors
+            scoreField.SetText(score.ToString());
         }
     }
 }
